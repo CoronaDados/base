efc711b9452b78c1e516949a5ea98652435ab14a
Eric S. Lucinger Ruiz <eu@ericruiz.com.br>
2020-03-21 01:35:01 -0300

    ignoring files

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/hello', 'HelloController@index')->name('hello');
+
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
