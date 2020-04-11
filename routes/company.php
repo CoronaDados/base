7282c02cf5e36cb2fdb0bc0c63272c298da0fa71
Willian Maria <willian.maria@sc.senai.br>
2020-04-11 15:52:28 -0300

    Adicionado tela de Dicas sobre o Corona

--- routes/company.php
+++ routes/company.php
@@ -5,23 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-//Route::get('login', 'Auth\LoginController@showCompanyLoginForm')->name('login');
-//Route::post('login', '\App\Http\Controllers\Auth\LoginController@companyLogin');
-
-
-//Route::get('/home', 'HomeController@index')->name('home');
-
 Route::group(['middleware' => 'auth:company'], function () {
 
     Route::get('users/import', 'UserController@viewImport')->name('users.import');
     Route::post('users/import', 'UserController@import')->name('users.import');
     Route::resource('users', 'UserController');
 
-    Route::resource('roles','RoleController');
-
-
+    Route::resource('roles', 'RoleController');
 
     Route::get('/', 'CompaniesController@dashboard')->name('home');
+    Route::get('/tips', 'CompaniesController@tips')->name('tips');
     Route::get('person/add', 'CompaniesController@addPerson')->name('person.create');
     Route::post('person/add', 'CompaniesController@storePeople')->name('person.store');
     Route::post('person/multi/monitoring', 'CompaniesController@multiMonitoring')->name('person.multi.monitoring');
@@ -33,22 +26,7 @@ Route::group(['middleware' => 'auth:company'], function () {
     Route::get('import2', 'CompaniesController@importView2');
     Route::post('import2', 'CompaniesController@import2')->name('import2');
 
-    Route::get('files/{file}', function ($file){
-        return response()->download(storage_path('files/'.$file));
+    Route::get('files/{file}', function ($file) {
+        return response()->download(storage_path('files/' . $file));
     })->name('files');
-
-/*
-    Route::get('test', function (){
-
-        $datas =  auth('company')->user()->persons()->with('casePeopleDay')->get();
-        foreach ($datas as $data){
-            if($data->casePeopleDay()->exists()){
-                $person[] = $data;
-            }
-        }
-        dd($person);
-    });*/
-
 });
-
-
