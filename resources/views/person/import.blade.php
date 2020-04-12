575569509a6ce873c7ca77ec77e40ed8253b68d1
Willian Maria <willian.maria@sc.senai.br>
2020-04-12 13:38:44 -0300

    Removido telas de usuários e deixado somente colaboradores

--- resources/views/person/import.blade.php
+++ resources/views/person/import.blade.php
@@ -4,19 +4,30 @@
     <div class="container-fluid pb-8 pt-3 pt-md-7">
         <div class="card  mt-3">
             <div class="card-header">
-                Importação de Colaboradores
+                Importação de admin, líderes e colaboradores
 
                 <div class="card-header">
                     <a class="btn btn-primary" href="{{route('company.files', 'modelo_person.xlsx')}}">Download do modelo</a>
                 </div>
             </div>
             <div class="card-body">
-                <form action="{{ route('person.import') }}" class="form-group" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('person.import') }}" class="form-group" method="POST"
+                      enctype="multipart/form-data">
                     @csrf
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-6">
                             <div class="form-group">
-                                <input type="file" name="file" class="form-control" required>
+                                <input type="file" name="file" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select name="role" class="form-control" required>
+                                    <option value="" disabled selected>Tipo de usuário</option>
+                                    @foreach($roles as $role)
+                                        <option value="{{$role->name}}">{{$role->name}}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
                     </div>
