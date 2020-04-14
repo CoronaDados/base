c81eb8d382463d11ffba80914a588dac2e78e881
Bruno Medeiros <bruno.medeiros@sc.senai.br>
2020-04-13 22:04:07 -0300

    Ajustes de layout na tela de register e login

--- resources/views/company/auth/login.blade.php
+++ resources/views/company/auth/login.blade.php
@@ -7,21 +7,8 @@
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
                 <div class="card bg-secondary shadow border-0">
-                    <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>Entre com</small></div>
-                        <div class="btn-wrapper text-center">
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
-                                <span class="btn-inner--text">{{ __('Google') }}</span>
-                            </a>
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/linkedin1.svg"></span>
-                                <span class="btn-inner--text">{{ __('LinkedIn') }}</span>
-                            </a>
-                        </div>
-                    </div>
                     <div class="card-body px-lg-5 py-lg-5">
-
+                        <div class="text-muted text-center mt-2 mb-3"><small>Bem vindo!</small></div>
                         <form role="form" method="POST" action="{{ route('company.login') }}">
                             @csrf
 
--- resources/views/company/auth/register.blade.php
+++ resources/views/company/auth/register.blade.php
@@ -8,19 +8,6 @@
         <div class="row justify-content-center">
             <div class="col-lg-6 col-md-8">
                 <div class="card bg-secondary shadow border-0">
-{{--                    <div class="card-header bg-transparent pb-5">--}}
-{{--                        <div class="text-muted text-center mt-2 mb-4"><small>{{ __('Sign up with') }}</small></div>--}}
-{{--                        <div class="text-center">--}}
-{{--                            <a href="#" class="btn btn-neutral btn-icon mr-4">--}}
-{{--                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>--}}
-{{--                                <span class="btn-inner--text">{{ __('Github') }}</span>--}}
-{{--                            </a>--}}
-{{--                            <a href="#" class="btn btn-neutral btn-icon">--}}
-{{--                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>--}}
-{{--                                <span class="btn-inner--text">{{ __('Google') }}</span>--}}
-{{--                            </a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
                             <small>{{ __('Cadastre sua empresa') }}</small>
@@ -119,7 +106,7 @@
                                     <div class="custom-control custom-control-alternative custom-checkbox">
                                         <input class="custom-control-input" required id="customCheckRegister" type="checkbox">
                                         <label class="custom-control-label" for="customCheckRegister">
-                                        <span class="text-muted">{{ __('Eu concordo com os') }} <a href="{{ route('termos-de-privacidade') }}">{{ __('Termos de Privacidade') }}</a></span>
+                                        <span class="text-muted">{{ __('Eu concordo com os') }} <a href="#!" data-toggle="modal" data-target="#termoPrivacidade">{{ __('Termos de Privacidade') }}</a></span>
                                         </label>
                                     </div>
                                 </div>
@@ -135,6 +122,61 @@
                         <a href="{{ route('company.login') }}" class="text-secondary">
                             <small>Voltar</small>
                         </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="termoPrivacidade" tabindex="-1" role="dialog" aria-labelledby="termoPrivacidade" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title" id="termoPrivacidade">Termo de Privacidade e de uso dos dados</h1>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body pt-0">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-12 col-md-7">
+                            <div class="card bg-secondary shadow border-0">
+                                <div class="card-body p-0">
+                                    <p>O Sistema FIESC, composto pelas entidades FIESC, SESI, SENAI, IEL e CIESC, através desta Política de
+                                        Privacidade (“Política”), reconhece a importância de garantir a segurança dos dados pessoais que são
+                                        compartilhados pelos usuários ao acessarem a aplicação Corona Dados. Além disso, a "Política"
+                                        esclarece quais as informações e os dados pessoais que serão coletados, além da forma que serão
+                                        tratados durante o uso da aplicação.</p>
+                                    <p>Ao fornecer seus dados pessoais, o usuário automaticamente declara conhecer e aceitar os termos desta
+                                        Política. Serão assim considerados dados como: nome, telefone, e-mail, data de nascimento, sexo,
+                                        números de CPF, CEP e se você faz parte ou não do grupo de riscos.</p>
+                                    <p>Os dados pessoais poderão ser utilizados para as seguintes finalidades:</p>
+                                    <ul>
+                                        <li class="font-weight-light">Manter atualizados os cadastros dos usuários para fins de contato pelos meios de comunicação
+                                            disponíveis, sendo correio eletrônico, sms, whatsapp e/ou telefone;</li>
+                                        <li class="font-weight-light">Elaborar estatísticas gerais, sem que haja identificação dos usuários com o objetivo de garantir
+                                            a proteção a vida;</li>
+                                        <li class="font-weight-light">Responder as dúvidas e solicitações dos usuários.</li>
+                                    </ul>
+                                    <p>Todos os dados pessoais coletados, são armazenados em base de dados por meio de mecanismos de
+                                        criptografia e segurança da informação nos padrões de mercado, sendo tratados e compartilhados
+                                        entre as entidades do Sistema FIESC e terceiros contratados para prestação de serviços. Estes
+                                        terceiros são requeridos a usarem a informação pessoal que compartilharmos com eles apenas para
+                                        prestarem os serviços que contratamos, em nosso nome e tratarem sua informação pessoal como
+                                        estritamente confidencial.
+                                    </p>
+                                    <p>Este termo tem como objetivo expressar o consentimento de uso de seus dados pessoais, por parte da
+                                        FIESC, que atuará como controlador de dados perante a Lei Geral de Proteção de dados (Lei 13.709).</p>
+                                    <p>As informações compartilhadas com a FIESC serão tratadas durante o período indeterminado, até que o
+                                        consentimento seja revogado.</p>
+                                    <p>O Sistema FIESC assegura que a política de privacidade possui regras claras, precisas e em concordância
+                                        com as leis vigentes.</p>
+                                    <p>O Sistema FIESC poderá alterar os termos da política de privacidade sem qualquer aviso prévio, de forma
+                                        que deverá ser verificada periodicamente.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
