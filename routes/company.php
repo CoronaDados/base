9f4116bc63adee0ea1e86e087025c23cc061cfe2
Willian Maria <willian.maria@sc.senai.br>
2020-04-15 15:45:57 -0300

    Adicionado informações de monitorados do dia

--- routes/company.php
+++ routes/company.php
@@ -14,6 +14,7 @@ Route::group(['middleware' => ['auth:company']], function () {
     Route::get('/help', 'CompaniesController@help')->name('help');
 
     Route::get('companies/monitoring', 'CompaniesController@monitoring')->name('monitoring');
+    // Route::post('companies/monitoring', 'CompaniesController@monitoring')->name('monitoring');
     Route::post('companies/monitoring/{id}', 'CompaniesController@storeMonitoring');
 
     Route::get('companies/monitoring/history', 'CompaniesController@monitoringHistory')->name('monitoring.history');
