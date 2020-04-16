7f13c0b16d56b2472d9d085ed3a45bd39d53ec98
Willian Maria <willian.maria@sc.senai.br>
2020-04-16 13:22:29 -0300

    Correções dos erros

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,7 +41,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        if (config('app.env') == 'production') {
+        if ($this->shouldReport($exception) && config('app.env') == 'production') {
             Mail::to(config('app.email_list_error'))->send(new ErrorMail($exception));
         }
 
