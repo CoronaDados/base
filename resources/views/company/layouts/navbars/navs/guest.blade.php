0b388ab9551010362515068f0436a499a1f85450
Willian Maria <willian.maria@sc.senai.br>
2020-04-22 16:55:06 -0300

    Corrigido assets argon

--- resources/views/company/layouts/navbars/navs/guest.blade.php
+++ resources/views/company/layouts/navbars/navs/guest.blade.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
     <div class="container px-4">
-{{--        <a class="navbar-brand" href="{{ route('home') }}">--}}
-{{--            <img src="/logo.png" />--}}
-{{--        </a>--}}
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {{--        <a class="navbar-brand" href="{{ route('home') }}">--}}
+        {{--            <img src="/logo.png" />--}}
+        {{--        </a>--}}
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar-collapse-main">
@@ -12,11 +13,13 @@
                 <div class="row">
                     <div class="col-6 collapse-brand">
                         <a href="#">
-                            <img src="{{ asset('argon') }}/img/brand/blue.png">
+                            <img src="/argon/img/brand/blue.png">
                         </a>
                     </div>
                     <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse"
+                            data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false"
+                            aria-label="Toggle sidenav">
                             <span></span>
                             <span></span>
                         </button>
